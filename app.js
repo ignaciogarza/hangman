25af607c249ffff8d0f03543ed3ecf313cd89925
Ignacio Garza <ignacioagarza@gmail.com>
2020-07-15 12:53:03 -0500

    Javascript to lister for keyboard input

--- app.js
+++ app.js
@@ -36,4 +36,39 @@ function displayWord() {
   }
 }
 
+//update the wrong letters
+function updateWrongLettersEl() {}
+
+// show notification
+function showNotification() {
+  notification.classList.add("show");
+  setTimeout(() => {
+    notification.classList.remove("show");
+  }, 2000);
+}
+
+// keydown letter press
+
+window.addEventListener("keydown", (e) => {
+  if (e.keyCode >= 65 && e.keyCode <= 90) {
+    const letter = e.key;
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+        updateWrongLettersEl();
+      } else {
+        showNotification();
+      }
+    }
+  }
+});
+
 displayWord();
